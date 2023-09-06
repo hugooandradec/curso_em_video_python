0031d3e1999fae9dc3494c30fde943cd21353396
Victor Hugo <hugooandradec@gmail.com>
2023-09-06 01:57:31 -0300

    Update ex073_tabela_brasileirão.py

--- Mundo 03/Exercícios/ex073_tabela_brasileirão.py
+++ Mundo 03/Exercícios/ex073_tabela_brasileirão.py
@@ -1,4 +1,4 @@
-"""Crie uma tupla preenchida com o 20 primeiros colocados da Tabela do Brasileirão, na ordem de colocação.
+"""Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Brasileirão, na ordem de colocação.
 Depois mostre:
 A - Apenas os 5 primeiros colocados.
 B - Os últimos 4 colocados da tabela.
