c77f85903563e156fa4c4e98d5c024b51bb37a70
Victor Hugo <hugooandradec@gmail.com>
2023-09-07 23:20:23 -0300

    finalização do ex077

--- Mundo 03/Exercícios/ex077_tuplas_palavras.py
+++ Mundo 03/Exercícios/ex077_tuplas_palavras.py
@@ -1,2 +1,13 @@
 """Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar,
 para cada palavra, quais são as suas vogais."""
+
+palavras = ('PERA', 'UVA', 'MAÇA', 'MAMAO', 'BANANA', 'ABACAXI', 'ABACATE', 'GOIABA', 'KIWI', 'MANGA')
+vogais = 'AEIOU'
+
+print()
+for palavra in palavras:
+    print(f'Na palavra {palavra} temos as vogais: ', end='')
+    for letra in palavra:
+        if letra in vogais:
+            print(letra, end=' ')
+    print()
