73a4e534277c9ea173355c05a07c0fef7e24717d
Victor Hugo <hugooandradec@gmail.com>
2023-09-07 21:15:01 -0300

    finalizando o exercício 074

--- Mundo 03/Exercícios/ex074_armazenando_números_tupla.py
+++ Mundo 03/Exercícios/ex074_armazenando_números_tupla.py
@@ -1,2 +1,13 @@
 """Crie um programa que vai gerar 5 números aleatórios e colocar em uma tupla. Depois disso, mostra a listagem
 de números gerados e também indique o menor e o maior valor que estão na tupla."""
+
+from random import randint
+
+numeros_aleatorios = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+
+print('Os valores sorteados foram: ', end='')
+for n in numeros_aleatorios:
+    print(f'{n} ', end='')
+
+print(f'\nO maior valor foi: {max(numeros_aleatorios)}\n'
+      f'O menor valor foi: {min(numeros_aleatorios)}')
